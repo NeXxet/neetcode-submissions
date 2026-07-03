5a0849df1d3c8b6340fcb7386da35dc6c465ff1d
NeXxet <56108498+NeXxet@users.noreply.github.com>
2026-07-03 00:11:52 -0500

    Add: path-sum - submission-3

---
+++ Data Structures & Algorithms/path-sum/submission-3.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        self.currentSum = 0
+
+        def dfs(root):
+            if not root:
+                return False
+            
+            self.currentSum += root.val
+
+            if self.currentSum == targetSum and not (root.left or root.right):
+                return True
+            if dfs(root.left):
+                return True
+            if dfs(root.right):
+                return True
+
+            self.currentSum -= root.val
+            return False
+        
+        return dfs(root)
+            
+
