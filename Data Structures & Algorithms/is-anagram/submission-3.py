66d5799c111394412896c17e690d62f866e1f701
NeXxet <56108498+NeXxet@users.noreply.github.com>
2026-07-10 18:04:24 -0500

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap = defaultdict(int)
+
+        for c in s:
+            hashmap[c] += 1
+        
+        for c in t:
+            hashmap[c] -= 1
+
+        for value in hashmap.values():
+            if value != 0:
+                return False
+
+        return True
+
