cf147853fcdda1898e36d0f48c3b5722a9513af6
NeXxet <56108498+NeXxet@users.noreply.github.com>
2026-07-23 18:06:58 -0500

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+
+        prefix = 1
+        for index, num in enumerate(nums):
+            result[index] = prefix
+            prefix *= num
+        
+        postfix = 1
+        for index in range(len(nums)-1, -1, -1):
+            result[index] = result[index] * postfix
+            postfix *= nums[index]
+
+        return result
