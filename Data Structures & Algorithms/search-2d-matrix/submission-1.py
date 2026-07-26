7e5259d0d3796ce5484c4bf03ed297d4c4d7e4aa
NeXxet <56108498+NeXxet@users.noreply.github.com>
2026-07-26 18:11:25 -0500

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # formula for getting equivalent position from matrix
+        # if numbers were in a single list
+        # (len(matrix[0]) * matrix row) + matrix column
+
+        # formula to convert numLineIndex to matrix representation
+        # row = numLineIndex // rowSize
+        # col = numLineIndex % rowSize
+
+        cols = len(matrix[0])
+
+        low = 0
+        high = (cols * len(matrix)) - 1
+
+        while low <= high:
+            mid = (high+low)//2
+
+            midCell = matrix[mid//cols][mid%cols]
+
+            if midCell < target:
+                low = mid + 1
+            elif midCell > target:
+                high = mid - 1
+            else:
+                return True
+
+        return False
