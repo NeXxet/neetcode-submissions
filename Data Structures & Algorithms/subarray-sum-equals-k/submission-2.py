4bf8440fdc62693d480c70ab92d9e874286f1cf4
NeXxet <56108498+NeXxet@users.noreply.github.com>
2026-07-31 00:31:52 -0500

    Add: subarray-sum-equals-k - submission-2

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prevPrefixes = defaultdict(int)
+        prevPrefixes[0] = 1
+        result = currentSum = 0
+
+        for num in nums:
+            currentSum += num
+            diff = currentSum - k
+            result += prevPrefixes[diff]
+            prevPrefixes[currentSum] += 1
+        
+        return result
