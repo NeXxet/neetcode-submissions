deca9eaf699c5f64a5008a9577e70687613ca8e4
NeXxet <56108498+NeXxet@users.noreply.github.com>
2026-08-08 16:42:56 -0500

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.wordPresent = False
+        rows = len(board)
+        cols = len(board[0])
+
+        def backtrack(i, j, k):
+            if k == len(word):
+                self.wordPresent = True
+
+            if i < 0 or i >= rows or j < 0 or j >= cols or self.wordPresent:
+                return 
+            if board[i][j] != word[k]:
+                return
+
+            cacheChar = board[i][j]
+            board[i][j] = None
+            
+            backtrack(i-1, j, k+1)
+            backtrack(i+1, j, k+1)
+            backtrack(i, j-1, k+1)
+            backtrack(i, j+1, k+1)
+
+            board[i][j] = cacheChar        
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0]:
+                    backtrack(i, j, 0)
+        
+        return self.wordPresent
