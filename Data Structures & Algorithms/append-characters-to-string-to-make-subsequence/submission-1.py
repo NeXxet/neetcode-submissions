565125b7cae241e441af7e6fae9ea55c6fb3001c
NeXxet <56108498+NeXxet@users.noreply.github.com>
2026-08-10 18:04:14 -0500

    Add: append-characters-to-string-to-make-subsequence - submission-1

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        # handle case where t is already subsequence of s
+        # if loop ends when t_pointer == len(t) 
+
+        s_ptr = 0
+        t_ptr = 0
+
+        while s_ptr < len(s) and t_ptr < len(t):
+            if s[s_ptr] == t[t_ptr]:
+                s_ptr += 1
+                t_ptr += 1
+            else:
+                s_ptr += 1
+
+        return len(t) - t_ptr
